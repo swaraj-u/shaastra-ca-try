6401b9d6d7619bdd63652abaa125a9613dcdc0cf
swaraj-u <swarajrupadhyay@gmail.com>
2024-07-13 23:30:22 +0530

    adding basename

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/shaastra-ca-try">
     <div className="App">
       <Routes>
        <Route path="/" element={<Home />} />
