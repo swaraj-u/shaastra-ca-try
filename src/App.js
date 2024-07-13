97af2c5217899d4b28e4f2d92662b307e6f15a8b
swaraj-u <swarajrupadhyay@gmail.com>
2024-07-13 23:26:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./Home";
+import Login from "./Login";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+       <Route path="/" element={<Home />} />
+       <Route path="/home" element={<Home />} />
+       <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
+    </Router>
   );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import './home.css';
+
+
+const Home = () => {
+
+    const tabContainer = document.querySelector("#faqs > section");
+
+    const [counter, setCounter] = useState(false);
+
+    const startDragging = () => {
+        setCounter(true);
+    }
+
+    const Dragging = (event) => {
+        tabContainer.scrollLeft -= event.movementX;
+    }
+
+    const stopDragging = () => {
+        setCounter(false);
+    }
+
+    const Nothing = () => {};
+
+    return ( 
+    <div id="home">
+        <div id="intro">
+            <img id="bg-image" alt="" src="https://i.pinimg.com/564x/20/db/0f/20db0ffb0c7b1417b96e458c3559bf43.jpg" />
+            <p id="intro-text">
+                <p>Be a</p>
+                <p>CAMPUS AMBASSADOR</p>
+                <p>Empower our voice and lead the way.</p>
+            </p>
+        </div>
+        <div id="about">
+            <div class="imgDiv" >
+                <img alt="" id="bigOneLeft" src="https://i.pinimg.com/originals/d8/ab/6f/d8ab6f036ad7fc6b207cd39e20e50845.jpg" />
+                <img alt="" id="smallOneLeft" src="https://i.pinimg.com/564x/c6/1c/dc/c61cdce503d83dde700867f3828e4e14.jpg"/>
+            </div>
+            <div id="about-text">
+            <h1>About Us</h1>
+            <p>Harry Potter is a series of seven fantasy novels written by British author J. K. Rowling. The novels chronicle the lives of a young wizard, Harry Potter, and his friends, Hermione Granger and Ron Weasley, all of whom are students at Hogwarts School of Witchcraft and Wizardry. </p><br></br>
+            <p>The series was mainly produced by David Heyman, and stars Daniel Radcliffe, Rupert Grint, and Emma Watson as the three leading characters: Harry Potter, Ron Weasley, and Hermione Granger. Four directors worked on the series: Chris Columbus, Alfonso Cuarón, Mike Newell, and David Yates. Michael Goldenberg wrote the screenplay for Harry Potter and the Order of the Phoenix (2007), while the remaining films' screenplays were written by Steve Kloves. Production took place over ten years, with the main story arc following Harry's quest to overcome his arch-enemy Lord Voldemort.</p>
+            </div>
+        </div>
+    </div> ); 
+}
+ 
+export default Home;
